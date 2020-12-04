1b4842706a86b98343d7e6ba2cb504ccc1e38259
Max <rudko.max10@gmail.com>
2020-12-04 10:14:07 +0200

    Use new context api

--- app/src/main/java/com/justsoft/nulpschedule/utils/ContextExtensions.kt
+++ app/src/main/java/com/justsoft/nulpschedule/utils/ContextExtensions.kt
@@ -3,15 +3,28 @@ package com.justsoft.nulpschedule.utils
 import android.app.NotificationManager
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.Context.CLIPBOARD_SERVICE
+import android.content.Context.*
+import android.os.Build.*
 import android.view.inputmethod.InputMethodManager
 
 val Context.clipboardManager: ClipboardManager
-    get() = this.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+    get() = if (VERSION.SDK_INT >= VERSION_CODES.M) {
+        this.getSystemService(ClipboardManager::class.java)
+    } else {
+        this.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+    }
 
 val Context.inputMethodManager: InputMethodManager
-    get() = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    get() = if (VERSION.SDK_INT >= VERSION_CODES.M) {
+        this.getSystemService(InputMethodManager::class.java)
+    } else {
+        this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
 val Context.notificationManager: NotificationManager
-    get() = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    get() = if (VERSION.SDK_INT >= VERSION_CODES.M) {
+        this.getSystemService(NotificationManager::class.java)
+    } else {
+        this.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+    }
 
